3425926ac568644d6376ae93308c5fb32cfe8f48
Hellieos <147742805+Hellieos@users.noreply.github.com>
2025-06-07 21:16:30 +0200

    Update cypress.config.js new

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,24 @@
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-require('dotenv').config();
 
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       console.log('Plugin setupNodeEvents called');
-      allureWriter(on, config)
+      allureWriter(on, config);
       console.log('Allure writer called');
-      config.env.API_URL = process.env.CYPRESS_API_URL;
-      config.env.API_TOKEN = process.env.CYPRESS_API_TOKEN;
-      config.env.TEAM_ID = process.env.CYPRESS_TEAM_ID;
+
+      // Assign environment variables (for both local + CI)
+      config.env.API_URL = process.env.CYPRESS_API_URL || config.env.API_URL;
+      config.env.API_TOKEN = process.env.CYPRESS_API_TOKEN || config.env.API_TOKEN;
+      config.env.TEAM_ID = process.env.CYPRESS_TEAM_ID || config.env.TEAM_ID;
+
+      // Set baseUrl if defined
+      if (process.env.CYPRESS_API_URL) {
+        config.baseUrl = process.env.CYPRESS_API_URL;
+      }
+
       return config;
     },
-    baseUrl: process.env.CYPRESS_API_URL,
     specPattern: 'cypress/e2e/**/*.cy.js',
   },
 };
